32a61631b2cd8241ce1771bdaf3c1d46c8d5456a
Adrian Américo <1fnadrian1@gmail.com>
2024-05-30 18:21:30 -0300

    feat: created apollo client

---
+++ src/data/graphql/queries/get-characters.ts
@@ -0,0 +1,23 @@
+import { gql } from '@apollo/client';
+
+export const GET_CHARACTERS = gql`
+ query Characters($name: String) {
+   characters(filter: { name: $name }) {
+     results {
+      id
+       name
+       species
+       status
+       type
+       gender
+       origin {
+         name
+       }
+       location {
+         name
+       }
+       image
+     }
+   }
+ }
+`;
