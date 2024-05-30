d83c213cbbec83c392f398569c8b4b130cde0d84
Adrian Américo <1fnadrian1@gmail.com>
2024-05-30 18:27:56 -0300

    feat: add atomic card-section component

---
+++ src/presentation/atomic/atm.card-section/index.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import { spacing } from '../obj.constants';
+
+export const CardSection = styled.section`
+display: grid;
+grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+gap: 1rem;
+padding: 1rem;
+width: 100%;
+transition: ease all 250ms;
+margin-top: ${spacing.large};
+`;
