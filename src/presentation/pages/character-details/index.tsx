f30f8ed0fe5b362f246e90da6372c302111a4ea8
Adrian Américo <1fnadrian1@gmail.com>
2024-05-30 23:12:09 -0300

    feat: add character-details-page

--- src/presentation/pages/character-details/index.tsx
+++ src/presentation/pages/character-details/index.tsx
@@ -1,3 +1,29 @@
+import { useMemo } from 'react';
+import { useParams } from 'react-router-dom';
+import { useGetCharactersEpisodes } from '@/domain/hooks';
+import { Header } from '@/presentation/atomic';
+import { CharacterEpisodesList } from './org.character-episodes-list';
+import { Main } from '../home/styles';
+
 export const CharacterDetails = () => {
-  return <div>Hello world</div>;
+  const params = useParams<{ id: string }>();
+  const { data, error, loading } = useGetCharactersEpisodes(params.id || '');
+
+  const character = useMemo(() => {
+    return data?.charactersByIds?.find((character) => character);
+  }, [data]);
+
+  return (
+    <>
+      <Header />
+
+      <Main>
+        <CharacterEpisodesList
+          character={character}
+          error={!!error}
+          loading={loading}
+        />
+      </Main>
+    </>
+  );
 };
